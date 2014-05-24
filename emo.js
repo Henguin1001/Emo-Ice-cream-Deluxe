c31b27d480e49bad6bbc7d6c4a906bdbad76c107
Henry Troutman <henguin1001@gmail.com>
2014-05-24 11:03:13 -0700

    express and socket.io

---
+++ emo.js
@@ -0,0 +1,60 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+	http = require('http'),
+	path = require('path');
+
+
+
+var app = express();
+
+// Twig init
+var Twig = require('twig'),
+	twig_ob = require('twig_obj');
+Twig.extend(twig_ob);
+
+// all environments
+app.set('port', process.env.PORT || 80);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'twig');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
+
+////////////
+// Routes //
+////////////
+
+
+var routes = require('./routes');
+
+app.get('/', routes.index);
+
+////////////////
+// End Routes //
+////////////////
+
+
+var server = http.createServer(app);
+
+// express
+server.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
+// socket.io
+var socket_io = require('socket.io'),
+	io = socket_io.listen(server);
+
+
