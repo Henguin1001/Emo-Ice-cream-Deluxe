a41d8a7680aaee8ca1fb19e8a78be641bff15d52
Henry Troutman <henguin1001@gmail.com>
2014-05-24 11:38:23 -0700

    templates

--- routes/express/index.js
+++ routes/express/index.js
@@ -1,3 +1,13 @@
-module.exports = function(app){
+module.exports = function(app) {
+	app.get('/', function(req, res) {
+		res.render('home',{
+			title:"Emo Ice Cream Deluxe"
+		});
+	});
+	app.get('/play', function(req, res) {
+		res.render('play',{
+			title:"play"
+		})
+	});
 
 };
